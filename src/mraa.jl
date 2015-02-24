20f1780c415c3db456881bbcdd9aa5b4e4238060
Steve Kelly <kd2cca@gmail.com>
2015-02-24 01:17:07 -0500

    change path to work on edison and UInt8 is 0.4

--- src/mraa.jl
+++ src/mraa.jl
@@ -1,9 +1,9 @@
 module mraa
 
-const libmraa = "/usr/local/lib/libmraa.so.0.6.0"
+const libmraa = "/usr/local/lib/i386-linux-gnu/libmraa.so.0.6.0"
 
 function platform_name()
-    bytestring(ccall((:mraa_get_platform_name, libmraa), Ptr{UInt8}, ()))
+    bytestring(ccall((:mraa_get_platform_name, libmraa), Ptr{Uint8}, ()))
 end
 
 
