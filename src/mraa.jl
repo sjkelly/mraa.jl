6ba99fe99cbdb6e4618faa7dce242dbac3410fee
Steve Kelly <kd2cca@gmail.com>
2015-02-28 17:18:09 -0500

    use sexy new enums... now we can party

--- src/mraa.jl
+++ src/mraa.jl
@@ -2,9 +2,14 @@ module mraa
 
 const libmraa = find_library(["libmraa"])
 
+include("enums.jl")
+
 function platform_name()
     bytestring(ccall((:mraa_get_platform_name, libmraa), Ptr{Uint8}, ()))
 end
 
+function platform_type()
+    Platform(ccall((:mraa_get_platform_type, libmraa), Cint, ()))
+end
 
 end # module
