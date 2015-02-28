dda6c69b989cd63095ad8010c19fabe3a76f67ab
Steve Kelly <kd2cca@gmail.com>
2015-02-28 16:04:48 -0500

    use find_library for locate libmraa location

--- src/mraa.jl
+++ src/mraa.jl
@@ -1,6 +1,6 @@
 module mraa
 
-const libmraa = "/usr/local/lib/i386-linux-gnu/libmraa.so.0.6.0"
+const libmraa = find_library(["libmraa"])
 
 function platform_name()
     bytestring(ccall((:mraa_get_platform_name, libmraa), Ptr{Uint8}, ()))
