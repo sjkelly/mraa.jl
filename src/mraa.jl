77ba841f9abb822ead94e682e5969b2626256cbc
Steve Kelly <kd2cca@gmail.com>
2015-03-02 22:10:07 -0500

    continue wrapping

--- src/mraa.jl
+++ src/mraa.jl
@@ -4,20 +4,28 @@ const libmraa = find_library(["/usr/local/lib/i386-linux-gnu/libmraa.so.0.6.0", 
 
 include("enums.jl")
 
+function set_priority(p)
+    Int(ccall((:mraa_set_priority, libmraa), Cint, (Cuint,), p))
+end
+
 function platform_name()
     bytestring(ccall((:mraa_get_platform_name, libmraa), Ptr{Uint8}, ()))
 end
 
-function version()
-    bytestring(ccall((:mraa_get_version, libmraa), Ptr{Uint8}, ()))
+function set_priority(p)
+    Int(ccall((:mraa_set_priority, libmraa), Cint, (Cuint,), p))
 end
 
-function platform_type()
+function version()
+    VersionNumber(bytestring(ccall((:mraa_get_version, libmraa), Ptr{Uint8}, ())))
+end
+
+function Platform()
     Platform(ccall((:mraa_get_platform_type, libmraa), Cint, ()))
 end
 
 function pin_count()
-    ccall((:mraa_get_pin_count, libmraa), Cuint, ())
+    Int(ccall((:mraa_get_pin_count, libmraa), Cuint, ()))
 end
 
 end # module
